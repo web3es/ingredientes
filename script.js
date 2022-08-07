ef2139734690f898ff13b9f6d40885455feb3c6e
mmednik <mmednik@gmail.com>
2022-08-07 19:00:12 -0300

    identificación individual de los ingredientes

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-const file = document.getElementById("file");
-const lang = "spa";
+const file = document.getElementById('file');
+const ingredients = document.getElementById('ingredients');
+const lang = 'spa';
 
+console.log(EDAMAM_APPID);
+console.log(EDAMAM_APPKEY);
 
 file.addEventListener('change', function(e){
   const worker = new Tesseract.TesseractWorker();
   worker.recognize(file.files[0].name, lang)
   .progress(function(packet){
-      console.info(packet)
-      //progressUpdate(packet)
+      if(packet.status=='recognizing text') {
+        ingredients.innerText = `Cargando... (${Math.round(packet.progress*100)}%)`;
+      }
   })
   .then(function(data){
-      console.log(data)
-      //progressUpdate({ status: 'done', data: data })
+      ingredients.innerText = '';
+
+      let text = data.text;
+      let arrIngredients = [];
+
+      text = text.replace('Ingredientes', '');
+      text = text.replace(':', '');
+      text = text.replace('.', '');
+
+      text.split(',').map(function(i) {
+        let cleanIngredient = i.trim();
+        cleanIngredient = cleanIngredient.replace('\n','');
+        cleanIngredient = cleanIngredient.replace('-','');
+        arrIngredients.push(cleanIngredient);
+      });
+
+      arrIngredients.map(function(i){
+        ingredients.innerHTML += `<li>${i}</li>`;
+      });
   })
 });
